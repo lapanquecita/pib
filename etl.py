e21c54550a23796d273964f7c629d8812648b865
Montse <79296842+lapanquecita@users.noreply.github.com>
2025-12-09 10:57:59 -0600

    Actualización datos

--- etl.py
+++ etl.py
@@ -213,7 +213,7 @@ def crear_estados():
     dfs = list()
 
     # Definimos la ruta base para los archivos de PIB por entidad.
-    ruta_base = "./source_estatal/conjunto_de_datos_pibe_entidad_{}2023_r.csv"
+    ruta_base = "./source_estatal/conjunto_de_datos_pibe_entidad_{}2024_p.csv"
 
     # Iteramos sobre cada entidad.
     for k, v in ENTIDADES.items():
